bdfbf777e36efc31e7e899d39b0adf1d757a37d3
Kihyeon Kim <gihyonkimu@gmail.com>
2024-04-28 19:21:34 -0400

    added logo and subtitle

--- react-portfolio/src/components/Layout/index.js
+++ react-portfolio/src/components/Layout/index.js
@@ -1,7 +1,12 @@
 import './index.scss'
+import Sidebar from '../Sidebar'
 
 const Layout = () => {
-  return <>My first project</>
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
 }
 
 export default Layout
